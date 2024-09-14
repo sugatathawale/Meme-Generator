d4e6ce9b06c92310a626746c408501f8d0361d22
sugat athawale <sugatathawale@sugats-MacBook-Pro.local>
2024-09-14 09:27:09 +0530

    Initial commit

---
+++ meme-generator/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Navbar from "./components/Navbar.js"
+import Form from "./components/Form.js"
+import Footer from "./components/Footer.js"
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Form />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
